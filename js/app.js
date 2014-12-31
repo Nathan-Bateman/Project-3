f91d9ac6f1065b10bf65f5cd9cf93ec8849e1af3
Nathan Bateman <nathan.bateman.jr@gmail.com>
2014-12-31 12:00:17 +0700

    made multiple bugs move at different speeds and player reset when reaching water

--- js/app.js
+++ js/app.js
@@ -1,20 +1,22 @@
 
-var playerStart_X = 200,
+var playerStart_X = 201,
     playerStart_Y = 440,
     playerLeftRight = 93,
-    playerUpDown = 75;
+    playerUpDown = 75,
+    enemySpeeds = [35,180,65,100, 120];
 
 
+//var randomitemfromarray = this[Math.floor(Math.random() * this.length)];
 // Enemies our player must avoid
-var Enemy = function() {
+var Enemy = function(addsubtractfromY) {
     // Variables applied to each of our instances go here,
     // we've provided one for you to get started
 
     // The image/sprite for our enemies, this uses
     // a helper we've provided to easily load images
-    this.x = 200;
-    this.y = 300;
-    this.speed = 20;
+    this.x = 0;
+    this.y = 300 + (addsubtractfromY);
+    this.speed = enemySpeeds[Math.floor(Math.random() * enemySpeeds.length)];
     this.sprite = 'images/enemy-bug.png';
 }
 
@@ -24,6 +26,10 @@ Enemy.prototype.update = function(dt) {
     // You should multiply any movement by the dt parameter
     // which will ensure the game runs at the same speed for
     // all computers.
+    if (this.x>500) {
+      this.x = -10;
+    };
+    this.x = this.x + ( this.speed * dt );
 }
 
 // Draw the enemy on the screen, required method for game
@@ -34,13 +40,17 @@ Enemy.prototype.render = function() {
 // Now write your own player class
 // This class requires an update(), render() and
 // a handleInput() method.
-var Player = function() {
-    this.x = playerStart_X;
-    this.y = playerStart_Y;
+var Player = function(x,y) {
+    this.x = x;
+    this.y = y;
     this.sprite = 'images/char-boy.png';
 }
-Player.prototype.update = function(dt){
 
+Player.prototype.update = function(dt){
+  if (this.y<0) {
+    this.x = playerStart_X;
+    this.y = playerStart_Y;
+  };
 }
 
 Player.prototype.render = function(){
@@ -81,14 +91,17 @@ Player.prototype.handleInput = function (input){
 } 
     };
 // Now instantiate your objects.
-var enemy1 = new Enemy;
+var enemy1 = new Enemy(0),
+    enemy2 = new Enemy(-155),
+    enemy3 = new Enemy(-79),
+    enemy4 = new Enemy(-240);
 
 
 // Place all enemy objects in an array called allEnemies
 var allEnemies = [];
-allEnemies.push(enemy1);
+allEnemies.push(enemy1,enemy2,enemy3,enemy4);
 // Place the player object in a variable called player
-var player = new Player;
+var player = new Player(playerStart_X,playerStart_Y);
 
 
 // This listens for key presses and sends the keys to your
